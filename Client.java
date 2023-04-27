07a7adce7f1b0dc67cb8c5d8686b14dad7f27962
L33t-Rose <jphillipjean@gmail.com>
2023-04-27 19:18:46 -0400

    got calculator to work

--- Client.java
+++ Client.java
@@ -1,14 +1,76 @@
 import java.net.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 import java.io.*;
 
 public class Client {
 	public static class Calculator {
+		
 		public static double calculate(String formula) {
-			System.out.println("We Got A FORMULA "+ formula);
-			String sanetizedFormula = formula.replaceAll(">|=|<", "");
-			System.out.println("SanititizedString: "+ sanetizedFormula);
+			System.out.println("We Got A FORMULA " + formula);
+			Queue<Double> rem = new LinkedList<Double>();
+			Queue<Character> ops = new LinkedList<>();
+			Stack<Double> numbers = new Stack<>();
+			Stack<Character> operations = new Stack<>();
+
+			String currentNumber = "";
+			for (int i = 1; i < formula.length() - 1; i++) {
+				char currentCharacter = formula.charAt(i);
+				// System.out.print(currentCharacter);
+				boolean isOperation = currentCharacter == '-' || currentCharacter == '+' || currentCharacter == '*'
+						|| currentCharacter == '/' || currentCharacter == '=';
+				if (currentCharacter == '=') {
+					rem.add(numbers.pop());
+				}
+				// Is it a number?
+				if (!isOperation) {
+					// If so, then we can add it currentNumber
+					currentNumber += currentCharacter;
+					if ("+-/*=".indexOf(formula.charAt(i + 1)) != -1) {
+						numbers.add(Double.parseDouble(currentNumber));
+						currentNumber = "";
+					}
+				} else {
+					operations.add(currentCharacter);
+				}
+				if (numbers.size() == 2) {
+					double b = numbers.pop();
+					double a = numbers.pop();
+					char sign = operations.pop();
+					if (sign == '/') {
+						numbers.add(a / b);
+					} else if (sign == '*') {
+						numbers.add(a * b);
+					} else if (sign == '-' || sign == '+') {
+						rem.add(a);
+						ops.add(sign);
+						numbers.add(b);
+					}
+
+				}
+
+			}
+			System.out.println(rem.size());
+			// double result = 0;
+			// String operations = sanetizedFormula.replaceAll("\\d","");
+			// while(!rem.isEmpty()){
+			// System.out.println(rem.poll());
+			// }
+			double res = rem.poll();
+			while (!ops.isEmpty()) {
+				char sign = ops.poll();
+				if (sign == '-') {
+					res -= rem.poll();
+				} else {
+					res += rem.poll();
+				}
+			}
+
+			System.out.println(res);
+
 			// WRITE CODE HERE
-			return 0; // UPDATE RETURN VALUE
+			return res; // UPDATE RETURN VALUE
 		}
 	}
 
@@ -41,7 +103,7 @@ public class Client {
 							state = 1;
 						}
 						break;
-					case 1://Packet Start - Start Receiving Numbers
+					case 1:// Packet Start - Start Receiving Numbers
 						boolean isNumber_state1 = validNumericDigits.indexOf(input) != -1;
 						if (isNumber_state1) {
 							packetizerState += input;
@@ -51,7 +113,7 @@ public class Client {
 							state = 6;
 						}
 						break;
-					case 2://Number - Start Receiving Numbers, Operation, Or Equal
+					case 2:// Number - Start Receiving Numbers, Operation, Or Equal
 						boolean isNumber_state2 = validNumericDigits.indexOf(input) != -1;
 						boolean isOperation_state2 = validOperations.indexOf(input) != -1;
 						if (isNumber_state2) {
@@ -67,42 +129,41 @@ public class Client {
 							state = 6;
 						}
 						break;
-					case 3://Operation Received - Start Receiving Numbers
+					case 3:// Operation Received - Start Receiving Numbers
 						boolean isNumber_state3 = validNumericDigits.indexOf(input) != -1;
-						if(isNumber_state3){
-							packetizerState+=input;
+						if (isNumber_state3) {
+							packetizerState += input;
 							state = 2;
-						}
-						else{
+						} else {
 							packetizerState = "";
 							state = 6;
 						}
 						break;
-					case 4://Equal Recived, Start Receiving End Packet
+					case 4:// Equal Recived, Start Receiving End Packet
 						boolean isEndPacket_state4 = input == '<';
-						if(isEndPacket_state4){
-							packetizerState+=input;
+						if (isEndPacket_state4) {
+							packetizerState += input;
 							state = 5;
-						}else{
+						} else {
 							packetizerState = "";
 							state = 6;
 						}
 						break;
-					case 5://End packet received, Start Receiving Start Packet For New Formula
+					case 5:// End packet received, Start Receiving Start Packet For New Formula
 						boolean isStartPacket_state5 = input == '>';
-						if(isStartPacket_state5){
+						if (isStartPacket_state5) {
 							formula = packetizerState;
 							packetizerState = "";
 							state = 1;
 						}
 						break;
-					case 6:// Error State, Start Trying To Receive Start Packet or 
-						packetizerState="";
+					case 6:// Error State, Start Trying To Receive Start Packet or
+						packetizerState = "";
 						boolean isStartPacket_state6 = input == '>';
-						if(isStartPacket_state6){
+						if (isStartPacket_state6) {
 							packetizerState += '>';
 							state = 1;
-						}else{
+						} else {
 							state = 0;
 						}
 						break;
@@ -110,7 +171,7 @@ public class Client {
 						return null;
 				}
 			}
-			if(state == 5){
+			if (state == 5) {
 				formula = packetizerState;
 				packetizerState = "";
 			}
